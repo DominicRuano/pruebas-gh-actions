eb03cfb0f645a9a79556e28231436c29ab73e9ac
DominicR <dojuparupe1975@gmail.com>
2024-12-08 00:29:37 -0600

    prueba final, 100 salkf;asdkfj

--- prueba/src/App.tsx
+++ prueba/src/App.tsx
@@ -22,7 +22,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR esto es una prueba 
+          Edit <code>src/App.tsx</code> and save to test HMR esto es una prueba de cosas
         </p>
       </div>
       <p className="read-the-docs">
